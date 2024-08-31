3f5cc947650a3438674ac40761b1bc506a8e0160
biswabhusan  sahoo <biswapvt506@gmail.com>
2024-08-31 20:10:58 +0530

    ok

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById('addTaskBtn').addEventListener('click', addTask);
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const taskList = document.getElementById('taskList');
+
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span>${taskText}</span>
+            <div>
+                <input type="checkbox" onclick="toggleTask(this)">
+                <button onclick="deleteTask(this)">Delete</button>
+            </div>
+        `;
+        taskList.appendChild(li);
+        
+    }
+    else if(taskText=='') {
+        alert("Oops! Please add a TASK")
+    }
+}
+
+function toggleTask(checkbox) {
+    const li = checkbox.parentElement.parentElement;
+    li.classList.toggle('completed');
+}
+
+function deleteTask(button) {
+    const li = button.parentElement.parentElement;
+    li.remove();
+}
